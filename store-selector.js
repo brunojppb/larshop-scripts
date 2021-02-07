1ff2a235141b56644216703127e3060f278c8ba4
Bruno Paulino <brunojppb@gmail.com>
2021-02-07 22:37:48 +0100

    correcao do hostname da liv mall

--- store-selector.js
+++ store-selector.js
@@ -195,7 +195,7 @@ window.addEventListener("DOMContentLoaded", function() {
         {
           name: 'Liv Mall ',
           url: 'https://larshoplivmall.commercesuite.com.br/',
-          hostname: 'www.larshoputilidades.com.br',
+          hostname: 'larshoplivmall.commercesuite.com.br',
         }
       ]
     },
